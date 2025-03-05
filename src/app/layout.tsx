2499389a7ade9b69892f19d16568354cc9c952be
LizjmH <Lizethjimenez399@outlook.es>
2025-03-05 09:06:49 -0600

    metadata generated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,13 @@ const geistMono = Geist_Mono({
     subsets: ['latin'],
 })
 
-export const metadata: Metadata = {
-    title: getTimeLeft().days + ' dias para talent land',
-    description: 'Cuanto Falta para Talent Land?',
+export const metadata: Metadata = {}
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: getTimeLeft().days + ' dias para talent land',
+        description: 'Cuanto Falta para Talent Land?',
+    }
 }
 
 export default function RootLayout({
